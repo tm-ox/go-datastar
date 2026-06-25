035d73f37a4edf41daaa48c58e2cbf24e369accd
tm <tm.oxenfree@gmail.com>
2026-06-25 15:03:43 +0700

    fix: card mb-4 static spacing

--- views/modules/card_templ.go
+++ views/modules/card_templ.go
@@ -283,7 +283,7 @@ func ProductCard(p *store.Product) templ.Component {
 		}
 		ctx = templ.ClearChildren(ctx)
 		if p != nil {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 18, "<div class=\"grid row-span-6 grid-rows-subgrid border border-border pb-4 backdrop rounded-lg overflow-hidden mb-4\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 18, "<div class=\"grid row-span-6 grid-rows-subgrid border border-border pb-4 backdrop rounded-lg overflow-hidden mb-2 md:mb-4\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
